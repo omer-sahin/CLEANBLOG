dc0ea711fd490d2ad3490caf588cc150135b3174
Ömer ŞAHİN <omers4hin04@gmail.com>
2023-01-30 23:51:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,45 @@
-const express=require("express")
-const ejs=require("ejs")
-const mongoose=require("mongoose");
-const Post=require("./models/Post")
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
+const methodOverride = require("method-override");
+const controller = require("./controller/controller");
 
+const port = 3000;
+const host = "127.0.0.1";
 
-const port=3000;
-const  host="127.0.0.1"
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cleanblog-db")
+  .then(() => console.log("Connected!"));
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-db')
-  .then(() => console.log('Connected!'));
- 
-const app=express();
+const app = express();
 
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(express.static("public"))
-app.set('view engine', 'ejs');
+app.get("/", controller.get);
+app.get("/about", controller.about);
+app.get("/addpost", controller.addPosts);
+app.get("/post", controller.posts);
 
-app.get("/",async (req,res)=>{
-    await Post.find({}).then(post=>{
-        res.render("index",{post:post})
+app.post("/newpost", controller.newposts);
+app.get("/posts/:id", controller.OnePosts);
 
-    })
+app.get("/edit", controller.editPosts);
 
+app.get("/posts/update/:id", controller.getUpdatePosts);
 
-    
-})
+app.put("/posts/:id", controller.UpdatePosts);
 
-app.get("/about",(req,res)=>{
-    res.render("about")
-
-})
-app.get("/addpost",(req,res)=>{
-    res.render("add_post")
-
-})
-app.get("/post",(req,res)=>{
-    res.render("post")
-})
-
-app.post("/newpost",async (req,res)=>{
-   await Post.create(req.body)
-    res.redirect("/")
-
-})
-app.get("/posts/:id",async (req,res)=>{
-    await Post.findById(req.params.id).then(post=>{
-        res.render("post",{post})
-    })
-
-})
-app.listen(port,host,()=>{
-    console.log(`Server Online ... http://${host}:${port}`)
-})
+app.delete("/posts/:id", controller.deletePosts);
+app.listen(port, host, () => {
+  console.log(`Server Online ... http://${host}:${port}`);
+});
